32945dfaea79262ae5dafb5141e4321c57ad9900
mjb2 <mjb2@CSG21-03.cs.ucc.ie>
2015-02-26 16:26:55 +0000

    updated files, add event

--- src/java/algorithm/FindMeeting.java
+++ src/java/algorithm/FindMeeting.java
@@ -5,6 +5,11 @@
 package algorithm;
 
 import dbpackage.DatabaseClass;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class FindMeeting {
@@ -13,6 +18,8 @@ public class FindMeeting {
     private String[] groupMembers;
     private final ArrayList<String> errors;
     private final DatabaseClass database;
+    private Connection connectionObject;
+    private Statement statementObject;
     
     public FindMeeting( ) {
         this.errors = new ArrayList<>( );
@@ -36,12 +43,92 @@ public class FindMeeting {
            return true;
      }
 
-     public String[] getGroupMembers() {
-         groupMembers = database.SelectColumn( "Select user_id FROM users;" );
-
-         return groupMembers;
+     public String[] getGroupMembers(String stream) {
+         String[] userIds;
+         userIds = database.SelectRow("SELECT user_id FROM users WHERE stream = '"+ stream +"'");
+         return userIds;
+     }
+//     public String[] getGroupMembers() throws SQLException {
+//         String[] users = {""};
+//        // Establish connection to database
+//        connectionObject = DriverManager.getConnection( "jdbc:mysql://" + "cs1.ucc.ie" + "/" + "2016_mjb2", "mjb2", "diechoro" );
+//        
+//        try{
+//            statementObject = connectionObject.createStatement( );
+//            ResultSet statementResult = statementObject.executeQuery( "Select user_id FROM users;" );
+//            while( statementResult.next( ) ){
+//                  String group = statementResult.getString( 1 );         
+//            }
+//        }
+//        catch( SQLException exceptionObject ){
+//           
+//        }
+//        finally{
+//               return users;
+//        }
+//         
+//         
+//    
+//         return groupMembers;
+//     }
+     
+     public int[] getLectureTimes(String startDate){
+         String member = groupMembers[0];
+         
+         int[] lectureTimes = database.SelectIntColumn("SELECT period FROM events JOIN has_events JOIN users ON events.event_id = has_events.event_id "
+                 + "AND users.user_id = has_events.user_id WHERE events.start_date = '" + startDate + "' AND users.user_id = '" + member + "' AND events.event_type = "
+                 + "'Lecture'"); 
+          
+        
+         return lectureTimes;
+     }
+     
+     public int[] getMembersEvents(String startDate, int memberNumber){
+         String member = groupMembers[memberNumber];
+         
+         int[] otherEvents = database.SelectIntColumn("SELECT period FROM events JOIN has_events JOIN users ON events.event_id = has_events.event_id "
+                 + "AND users.user_id = has_events.user_id WHERE events.start_date = '" + startDate + "' AND users.user_id = '" + member + "' AND events.event_type != "
+                 + "'Lecture'"); 
+          
+        
+         return otherEvents;
      }
   
+     public int getFreeSlot(String start_date, String stream){
+         String[] groupMembers = getGroupMembers(stream);
+         int memberNumber = 0;
+         int[] lectureTimes = getLectureTimes(start_date);
+         int[] eventTimes = getMembersEvents(start_date, memberNumber);
+         int currentFreeTime = -1;
+         
+         while( memberNumber < groupMembers.length ){
+             for(int period = 0; period < 9 ; period++){
+                 for( int i = 0; i < lectureTimes.length; i++){
+                     if(period == lectureTimes[i]){
+                         memberNumber = 0;
+                         break;
+                     }
+                     else{
+                            for( int j = 0; j < eventTimes.length; j++){
+                                if(period != eventTimes[j]){
+                                    if( memberNumber == groupMembers.length - 1){
+                                           currentFreeTime = period;
+                                           return currentFreeTime;
+                                    }else{
+                                           memberNumber++;
+                                    }                                    
+                                    break;
+                                }
+                            }
+                        }
+                     break;  
+                  }
+               break;                
+             }
+         }
+         return currentFreeTime;
+         
+     }
     /**
      * Form used to get the information of the type of meeting to be set and the preferred day to meet.
      * If the form user is a lecturer, they can make a meeting with a full class. However, if they are 
@@ -49,7 +136,7 @@ public class FindMeeting {
      * @return form
      */
     public String findMeetingForm( String userId ) {
-        String form = "<form name=\"find_meeting\" action=\"find_meeting.jsp\" method=\"POST\">\n";
+        String form = "<form name=\"find_meeting\" action=\"timetable.jsp\" method=\"POST\">\n";
 
         form += "<label for=\"eventType\">Event Type:</label>\n"
               + "<select name =\"eventType\" >\n +"
@@ -82,13 +169,12 @@ public class FindMeeting {
             //offer to set meeting with these students.
             //this.stream = database.
 
-            String[] members = getGroupMembers(  );
-            System.out.println( members.toString( ) );     
+            
 
          }
 
         form += "<label for=\"startDate\">Preferred Day:</label>\n"
-              + "<input type=\"text\" class=\"datepicker\" name=\"startDate\" value=\"" + "" + "\" placeholder=\"2015/01/01\"/><br />\n"
+              + "<input type=\"text\" class=\"datepicker\" name=\"startDate\" value=\"\" placeholder=\"2015/01/01\"/><br />\n"
               + "<label for=\"recurrence\">Recurrence:</label>\n"
               + "<select name=\"recurrence\" id='dropdown'>" 
               + "  <option value=\"once\" selected>Single Meeting</option>" 
@@ -97,7 +183,7 @@ public class FindMeeting {
               + "</select><br />";
 
 
-        form += "<input type='submit' value='Search Availability' name='submit' /><br />\n";
+        form += "<input type='submit' value='Search Availability' name='find_meet' /><br />\n";
         form += "</form>\n";
 
         return form;
